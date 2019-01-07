edbcb32ab7a30a1e572e87d533c256e0e6cb566d
Phil Höfer <phil@suma-ev.de>
2019-01-07 09:39:20 +0100

    Add key input

--- www/app/index.js
+++ www/app/index.js
@@ -1,2 +1,9 @@
 function boot () {
+    var storage = window.localStorage;
+    document.getElementById("key-input").value = storage.getItem("key");
+}
+
+function saveKey (value) {
+    var storage = window.localStorage;
+    storage.setItem("key", value);
 }
